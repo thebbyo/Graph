a5ad6e9117faa29df7684b86b6d1460fe35590e5
Diponker Roy Dibbyo <115203982+thebbyo@users.noreply.github.com>
2023-08-02 11:54:53 +0600

    Add files via upload

--- kosarajus.cpp
+++ kosarajus.cpp
@@ -1,83 +1,72 @@
-#include <iostream>
-#include <algorithm>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-class Graph{
-    int v;
-    vector<vector<int> >adj;
-    public:
-        Graph(int v): v(v), adj(v){}
-    void addEdge(int u, int w){
-        adj[u].push_back(w);
-    }
-  
-void DFS(int v, vector<bool> &visited, stack<int> &finishedStack) {
-    visited[v] = true;
+const int N = 20000;
+vector<int> adj[N];
+vector<int> transpose[N];
+int vis[N], transVis[N];
+int n, m;
+stack<int> finishedStack;
+
+void dfs(int v) {
+    vis[v] = true;
     for (auto neighbour : adj[v]) {
-        if (!visited[neighbour])
-            DFS(neighbour, visited, finishedStack);
+        if (!vis[neighbour]) dfs(neighbour);
     }
     finishedStack.push(v);
 }
 
+void dfsTrans(int v, vector<int> &scc) {
+    transVis[v] = true;
+    scc.push_back(v);
+    for (auto neighbour : transpose[v]) {
+        if (!transVis[neighbour]) dfsTrans(neighbour, scc);
+    }
+}
 
-    void DFSutil(int v, vector<bool> &visited, vector<int> &scc){
-        visited[v] = true;
-        scc.push_back(v);
-        for(auto neighbour:adj[v]){
-            if(!visited[neighbour]) DFSutil(neighbour, visited, scc);
+void getTranspose() {
+    for (int i = 0; i < n; i++) {
+        for (auto neighbour : adj[i]) {
+            transpose[neighbour].push_back(i);
         }
-
     }
+}
 
-    Graph getTranspose(){
-        Graph transpose(v);
-        for(int i =0;i<v;i++){
-            for(auto neighouur:adj[i]) transpose.addEdge(neighouur, i);
+void printSCC(vector<int> &scc) {
+    for (auto x : scc) cout << x << " ";
+    cout << endl;
+}
+
+void printSCCs() {
+    for (int i = 0; i < n; i++) {
+        if (!vis[i]) dfs(i);
+    }
+    getTranspose();
+    while (!finishedStack.empty()) {
+        int v = finishedStack.top();
+        finishedStack.pop();
+        if (!transVis[v]) {
+            vector<int> scc;
+            dfsTrans(v, scc);
+            printSCC(scc);
         }
-        return transpose;
+    }
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+
+    cin >> n >> m; // Number of vertices and edges in the graph
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
     }
 
-    void printSCCs(){
-        vector<bool> visited(v, false);
-        stack<int> finishedStack;
-        for(int i = 0;i<v; i++){
-            if(!visited[i]) DFS(i, visited, finishedStack);
-        }
-        
-        Graph transpose = getTranspose();
-        fill(visited.begin(), visited.end(), false);
-
-        while (!finishedStack.empty()){
-            int v = finishedStack.top();
-            finishedStack.pop();
-            if(!visited[v]){
-                vector<int> scc;
-                transpose.DFSutil(v, visited, scc);
-                printSCC(scc);
-            }
-        }  
-    }
-
-    void printSCC(vector<int> &scc){
-        for(auto x:scc) cout<<x<<" ";
-        cout<<endl;
-    }
-};
-
-int main(){
-    Graph graph(6);
-
-    graph.addEdge(0,1);
-    graph.addEdge(1,2);
-    graph.addEdge(2,0);
-    graph.addEdge(3,2);
-    graph.addEdge(3,4);
-    graph.addEdge(4,2);
-    graph.addEdge(3,5);
-
-    graph.printSCCs();
+    printSCCs();
 
     return 0;
 }
